546f1f5209e3688daf933b85a5c47d442e574277
Wang Weijing <you@example.com>
2019-07-26 16:05:00 +1000

    add particle background and rank tab

--- src/App.js
+++ src/App.js
@@ -1,15 +1,39 @@
 import React from 'react';
+import Particles from 'react-particles-js';
 import Navigation from './components/Navigation/Navigation';
 import Logo from './components/Logo/Logo';
+import Rank from './components/Rank/Rank'
 import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm'
 import './App.css';
+
+const ParticlesOption = {
+  "particles": {
+    "number": {
+      "value": 150
+    },
+    "size": {
+      "value": 3
+    }
+  },
+  "interactivity": {
+    "events": {
+      "onhover": {
+        "enable": true,
+        "mode": "repulse"
+      }
+    }
+  }
+}
 
 
 function App() {
   return (
     <div className="App">
+      <Particles className ="particles"
+        params={ParticlesOption} />
       <Navigation />
       <Logo />
+      <Rank />
       <ImageLinkForm />
       {/* <FaceRecognition /> */}
     </div>
